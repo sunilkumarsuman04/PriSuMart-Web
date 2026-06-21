b07b1ccecbd0aa387116fa684ac521d679c14a76
Sunil Kumar <sunilkumarsuman7070@gmail.com>
2026-06-21 11:17:43 +0530

    fix unused imports

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,9 @@ import { Hero } from '../components/home/Hero';
 import { Features } from '../components/home/Features';
 import { HowItWorks } from '../components/home/HowItWorks';
 import { Categories } from '../components/home/Categories';
-import { WhyChooseUs } from '../components/home/WhyChooseUs';
+// import { WhyChooseUs } from '../components/home/WhyChooseUs';
 import { AppPreview } from '../components/home/AppPreview';
-import { OtherApps } from '../components/home/OtherApps';
+// import { OtherApps } from '../components/home/OtherApps';
 import { About } from '../components/home/About';
 import { Contact } from '../components/home/Contact';
 
